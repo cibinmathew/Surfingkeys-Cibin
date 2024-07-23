2bad285a344592cd05cf4b5afcfdb34725a63300
Cibin Mathew <cibinmathew@users.noreply.github.com>
2024-07-23 22:00:25 +0200

    Added closeMatchingTabs
    Added closeMatchingTabs
    
    Added closeMatchingTabs
    
    Added closeMatchingTabs

--- src/content_scripts/common/default.js
+++ src/content_scripts/common/default.js
@@ -762,6 +762,14 @@ export default function(api, clipboard, insert, normal, hints, visual, front) {
     mapkey('gU', '#4Go to root of current URL hierarchy', function() {
         window.location.href = window.location.origin;
     });
+
+    mapkey('cx', '#3 close all tabs matching url pattern', function() {
+        let url = prompt("kill all tabs for matching url patterns \neg: https://*.google.com/foo*bar or \n*://mail.google.com/* ", location.href + "/*");
+        if (url != null) {
+            RUNTIME("closeMatchingTabs", { url: url });
+        }
+    });
+
     mapkey('gxt', '#3Close tab on left', function() {
         RUNTIME("closeTabLeft");
     });

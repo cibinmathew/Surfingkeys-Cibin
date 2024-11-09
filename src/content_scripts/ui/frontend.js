a519534831cbfc26cdb5e1d374db0b357b166fca
brook hong <hzgmaxwell@hotmail.com>
2024-11-09 09:19:54 +0800

    Merge all changes for MV3 and make it buildable for both MV2 and MV3

--- src/content_scripts/ui/frontend.js
+++ src/content_scripts/ui/frontend.js
@@ -8,6 +8,7 @@ import {
     getWordUnderCursor,
     htmlEncode,
     initL10n,
+    initSKFunctionListener,
     refreshHints,
     setSanitizedContent,
     mapInMode
@@ -21,6 +22,7 @@ import createNormal from '../common/normal.js';
 import createVisual from '../common/visual.js';
 import createHints from '../common/hints.js';
 import createAPI from '../common/api.js';
+import createDefaultMappings from '../common/default.js';
 import createOmnibar from './omnibar.js';
 import createCommands from './command.js';
 // import createCommands from './cbn_command.js';
@@ -49,6 +51,7 @@ const Front = (function() {
     };
 
     const api = createAPI(clipboard, insert, normal, hints, visual, self, {});
+    createDefaultMappings(api, clipboard, insert, normal, hints, visual, self);
 
     var _actions = self._actions,
         _callbacks = {};
@@ -221,7 +224,6 @@ const Front = (function() {
                 } else {
                     clearInterval(inputGuard);
                 }
-                console.log(inputGuard);
             }, 100);
         }
     };
@@ -297,6 +299,7 @@ const Front = (function() {
             }
         });
     };
+    self.chooseTab = _actions['chooseTab'];
 
     function buildUsage(metas, cb) {
         var feature_groups = [
@@ -396,10 +399,6 @@ const Front = (function() {
     _actions['showPopup'] = function(message) {
         showPopup(message.content);
     };
-
-    document.addEventListener("surfingkeys:showPopup", function(evt) {
-        showPopup(...evt.detail);
-    });
 
     self.vimMappings = [];
     let _aceEditor = null;
@@ -509,9 +508,6 @@ const Front = (function() {
     _actions['showBanner'] = function(message) {
         showBanner(message.content, message.linger_time);
     };
-    document.addEventListener("surfingkeys:showBanner", function(evt) {
-        showBanner(...evt.detail);
-    });
     _actions['showBubble'] = function(message) {
         var pos = message.position;
         pos.left += pos.winX;
@@ -575,8 +571,15 @@ const Front = (function() {
         StatusBar.show(message.contents, message.duration);
     };
 
-    document.addEventListener("surfingkeys:showStatus", function(evt) {
-        StatusBar.show(...evt.detail);
+    initSKFunctionListener("front", {
+        showPopup,
+        showBanner,
+        openFinder: () => {
+            Find.open();
+        },
+        showStatus: (contents, duration) => {
+            StatusBar.show(contents, duration);
+        },
     });
 
     self.toggleStatus = function(visible) {
@@ -1096,7 +1099,7 @@ function createAceEditor(normal, front) {
         };
         vim.defineEx("wq", "wq", wq);
         vim.defineEx("x", "x", wq);
-        vim.map('<CR>', ':wq', 'normal');
+        vim.map('<CR>', ':wq<CR>', 'normal');
         vim.defineEx("bnext", "bn", function(cm, input) {
             front.contentCommand({
                 action: 'nextEdit',
@@ -1184,17 +1187,17 @@ function createAceEditor(normal, front) {
                 vim.unmap('<CR>', 'insert');
                 vim.unmap('<C-CR>', 'insert');
                 if (message.type === 'url') {
-                    vim.map('<CR>', ':wq', 'insert');
+                    vim.map('<CR>', '<Esc>:wq<CR>', 'insert');
                     _ace.setOption('showLineNumbers', false);
                     _ace.language_tools.setCompleters([createUrlCompleter()]);
                     _ace.container.style.height = "30%";
                 } else if (message.type === 'input') {
-                    vim.map('<CR>', ':wq', 'insert');
+                    vim.map('<CR>', '<Esc>:wq<CR>', 'insert');
                     _ace.setOption('showLineNumbers', false);
                     _ace.language_tools.setCompleters([pageWordCompleter]);
                     _ace.container.style.height = "16px";
                 } else {
-                    vim.map('<C-CR>', ':wq', 'insert');
+                    vim.map('<C-CR>', '<Esc>:wq<CR>', 'insert');
                     _ace.setOption('showLineNumbers', true);
                     _ace.language_tools.setCompleters([pageWordCompleter]);
                     _ace.container.style.height = "30%";
